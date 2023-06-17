87c9e06fce0bb2e2f22e107a0cdd95699f5ed219
ZINKEVICH Maxim <drmvz@mail.ru>
2023-06-17 21:19:23 +0300

    fin

--- Task_60/Program.cs
+++ Task_60/Program.cs
@@ -8,28 +8,28 @@
 
 Console.Clear();
 
-int Prompt(string msg)
-{
-    Console.WriteLine(msg);
-    int number = int.Parse(Console.ReadLine());      //result[i,j] = new Random().Next(minValue, maxValue +1);
-    return number;
-}
+// int Prompt(string msg)
+// {
+//     Console.WriteLine(msg);
+//     int number = int.Parse(Console.ReadLine());      
+//     return number;
+// }
 
-int[,,] GetArray3D(int x, int y, int z, int minValue, int maxValue)
-{
-    int[,,] result = new int[x, y, z];
-    for(int i = 0; i < x; i++)
-    {
-        for(int j = 0; j < y; j++)
-        {
-            for(int k = 0; k < z; k++)
-            {
-                result[i,j,k] = new Random().Next(minValue, maxValue +1);
-            }
-        }
-    }
-    return result;
-}
+// int[,,] GetArray3D(int x, int y, int z, int minValue, int maxValue)
+// {
+//     int[,,] result = new int[x, y, z];
+//     for(int i = 0; i < x; i++)
+//     {
+//         for(int j = 0; j < y; j++)
+//         {
+//             for(int k = 0; k < z; k++)
+//             {
+//                 result[i,j,k] = new Random().Next(minValue, maxValue +1);
+//             }
+//         }
+//     }
+//     return result;
+// }
 
 void PrintArray3D(int[,,] array)
 {
@@ -46,13 +46,54 @@ void PrintArray3D(int[,,] array)
     }
     
 }
-int x = Prompt("Введите координаты X");
-int y = Prompt("Ввведите координаты Y");
-int z = Prompt("Ввведите координаты Z");
-int minValue = Prompt("Введите минимальное значение в массиве");
-int maxValue = Prompt("Введите максимальное значение в массиве");
-Console.WriteLine();
-int [,,] array3D = GetArray3D(x, y, z, minValue, maxValue);
-PrintArray3D(array3D);
+
+void CreateArray (int[,,] array3D)
+{
+    int[] temp = new int[array3D.Length];
+    int number;
+    for(int i = 0; i < temp.Length; i++)
+    {
+        temp[i] = new Random().Next(10,100);
+        number = temp[i];
+        if(i >= 1)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                while(temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+            }
+        }
+    }
+
+int count = 0;
+for(int x = 0; x < array3D.GetLength(0); x++)
+{
+    for(int y = 0; y < array3D.GetLength(1); y++)
+    {
+        for(int z = 0; z < array3D.GetLength(2); z++);   
+        array3D[x,y,z] = temp[count];
+        count++;
+    }
+}
+}
 
 
+
+// int x = Prompt("Введите координаты X");
+// int y = Prompt("Ввведите координаты Y");
+// int z = Prompt("Ввведите координаты Z");
+// int minValue = Prompt("Введите минимальное значение в массиве");
+// int maxValue = Prompt("Введите максимальное значение в массиве");
+// Console.WriteLine();
+// int [,,] array3D = GetArray3D(x, y, z, minValue, maxValue);
+
+// PrintArray3D(array3D);
+
+
+int[,,] array3D = new int[2,2,2];
+CreateArray (array3D);
+Console.Write($"{array3D}");
