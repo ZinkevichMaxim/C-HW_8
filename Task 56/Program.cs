9fe0976c72ebb3628526dac8905205886c310a4a
ZINKEVICH Maxim <drmvz@mail.ru>
2023-06-17 19:44:32 +0300

    fin

---
+++ Task 56/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.Clear();
+
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue +1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        Console.Write($"{array[i,j]}; ");
+        Console.WriteLine();
+    }
+    
+}
+
+int rows = Prompt("Введите колличество строк в массиве ");
+int columns = Prompt("Введите колличество столбцов в массиве ");
+int minValue = Prompt("Введите минимальное значение в массиве ");
+int maxValue = Prompt("Введите максимальное значение в массиве ");
+int [,] array = GetArray(rows, columns, minValue, maxValue);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+int rowNomber = GetRowNomber(array);
+Console.Write($"Наименьшая сумма элементов в строке {rowNomber}");
+
+
+int GetRowNomber(int [,] array)
+{
+    int row = 0;
+    int minSum = 0;
+    for(int i = 0; i < array.GetLength(1); i++)
+    {
+        minSum = minSum + array[0,i];                   // Посчитали минимальную сумму, как точку отсчета
+    }
+    Console.Write($"{minSum} ");
+    for(int i = 1; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i,j];
+        }
+        Console.Write($"{sum} ");
+    
+    if(minSum > sum)
+    {
+        minSum = sum;
+        row = i;
+    }
+    }
+    return row + 1;         // +1 чтобы вышел номер строки, а не индекс
+}
---
+++ Task_58/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.Clear();
+
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue +1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        Console.Write($"{array[i,j]}; ");
+        Console.WriteLine();
+    }
+    
+}
+
+
+
+int[,] MatrixMult(int[,] matrixA, int[,] matrixB)
+{
+    int rowsA = matrixA.GetLength(0);
+    int columnsB = matrixB.GetLength(1);
+    int[,] result = new int [rowsA, columnsB];
+    if(matrixA.GetLength(1) == matrixB.GetLength(0))
+    {
+        for(int i = 0; i < rowsA; i++)
+        {
+            for(int j = 0; j < columnsB; j++)
+            {
+                for(int n = 0; n < matrixA.GetLength(1); n++)
+                {
+                    result[i,j] += matrixA[i,n] * matrixB[n,j];
+                }
+            }
+        }
+    }
+    return result;
+}
+
+int rowsA = Prompt("Введите колличество строк в массиве A");
+int columnsA = Prompt("Введите колличество столбцов в массиве A");
+int minValueA = Prompt("Введите минимальное значение в массиве A");
+int maxValueA = Prompt("Введите максимальное значение в массиве A");
+int [,] matrixA = GetArray(rowsA, columnsA, minValueA, maxValueA);
+
+int rowsB = Prompt("Введите колличество строк в массиве B");
+int columnsB = Prompt("Введите колличество столбцов в массиве B");
+int minValueB = Prompt("Введите минимальное значение в массиве B");
+int maxValueB = Prompt("Введите максимальное значение в массиве B");
+int [,] matrixB = GetArray(rowsB, columnsB, minValueB, maxValueB);
+Console.WriteLine("Массив A");
+PrintArray(matrixA);
+Console.WriteLine("Массив B");
+PrintArray(matrixB);
+Console.WriteLine("Произведение двух матриц");
+int [,] result = MatrixMult(matrixA, matrixB);
+PrintArray(result);
